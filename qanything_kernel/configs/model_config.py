c6f80df895fd318defc1aa6e0b2ebcd215e6eee2
chenmins <chenmins@88.com>
2025-09-05 16:58:36 +0800

    个性化配置文件

--- qanything_kernel/configs/model_config.py
+++ qanything_kernel/configs/model_config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 # 获取环境变量GATEWAY_IP
-GATEWAY_IP = os.getenv("GATEWAY_IP", "localhost")
+GATEWAY_IP = os.getenv("GATEWAY_IP", "111.31.43.18")
 # LOG_FORMAT = "%(levelname) -5s %(asctime)s" "-1d: %(message)s"
 # logger = logging.getLogger()
 # logger.setLevel(logging.INFO)
@@ -165,13 +165,13 @@ VECTOR_SEARCH_SCORE_THRESHOLD = 0.3
 
 KB_SUFFIX = '_240625'
 # MILVUS_HOST_LOCAL = 'milvus-standalone-local'
-# MILVUS_PORT = 19530
+MILVUS_PORT = 19530
 MILVUS_HOST_LOCAL = GATEWAY_IP
-MILVUS_PORT = 19540
+# MILVUS_PORT = 19540
 MILVUS_COLLECTION_NAME = 'qanything_collection' + KB_SUFFIX
 
 # ES_URL = 'http://es-container-local:9200/'
-ES_URL = f'http://{GATEWAY_IP}:9210/'
+ES_URL = f'http://{GATEWAY_IP}:9200/'
 ES_USER = None
 ES_PASSWORD = None
 ES_TOP_K = 30
@@ -180,9 +180,9 @@ ES_INDEX_NAME = 'qanything_es_index' + KB_SUFFIX
 # MYSQL_HOST_LOCAL = 'mysql-container-local'
 # MYSQL_PORT_LOCAL = 3306
 MYSQL_HOST_LOCAL = GATEWAY_IP
-MYSQL_PORT_LOCAL = 3316
+MYSQL_PORT_LOCAL = 3306
 MYSQL_USER_LOCAL = 'root'
-MYSQL_PASSWORD_LOCAL = '123456'
+MYSQL_PASSWORD_LOCAL = 'chen1211'
 MYSQL_DATABASE_LOCAL = 'qanything'
 
 LOCAL_OCR_SERVICE_URL = "localhost:7001"
@@ -192,16 +192,16 @@ LOCAL_PDF_PARSER_SERVICE_URL = "localhost:9009"
 LOCAL_RERANK_SERVICE_URL = "localhost:8001"
 LOCAL_RERANK_MODEL_NAME = 'rerank'
 LOCAL_RERANK_MAX_LENGTH = 512
-LOCAL_RERANK_BATCH = 4
-LOCAL_RERANK_THREADS = 4
+LOCAL_RERANK_BATCH = 1
+LOCAL_RERANK_THREADS = 1
 LOCAL_RERANK_PATH = os.path.join(root_path, 'qanything_kernel/dependent_server/rerank_server', 'rerank_model_configs_v0.0.1')
 LOCAL_RERANK_MODEL_PATH = os.path.join(LOCAL_RERANK_PATH, "rerank.onnx")
 
 LOCAL_EMBED_SERVICE_URL = "localhost:9001"
 LOCAL_EMBED_MODEL_NAME = 'embed'
-LOCAL_EMBED_MAX_LENGTH = 1024
-LOCAL_EMBED_BATCH = 4
-LOCAL_EMBED_THREADS = 4
+LOCAL_EMBED_MAX_LENGTH = 512
+LOCAL_EMBED_BATCH = 1
+LOCAL_EMBED_THREADS = 1
 LOCAL_EMBED_PATH = os.path.join(root_path, 'qanything_kernel/dependent_server/embedding_server', 'embedding_model_configs_v0.0.1')
 LOCAL_EMBED_MODEL_PATH = os.path.join(LOCAL_EMBED_PATH, "embed.onnx")
 
